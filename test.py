9c1112a54d894a197b746b4f9527ddedf30f58d7
Jair Brown <brown.jair15@gmail.com>
2022-04-28 09:41:44 -0400

    added more to list of foods and print functions

--- test.py
+++ test.py
@@ -27,7 +27,40 @@ food_comp.append(str(seeds_nuts) + ' grams' + ' seeds and nuts.')
 food_comp.append(str(fruits) + ' grams' + ' fruits')
 
 
-for item in food_comp:
-    print ("Your dog needs " + item)
+def food_print(*food_comp):
+    for item in food_comp:
+        print('Your Canine friend needs, ' + item)
 
+food_print(*food_comp)
+
+
+print("Now that we know how much of each of these things your dog needs, lets find out what exactly they all are.")
+muscle_types = ["Chicken", 'Beef', 'Pork']
+fruit_types = ['Blueberries','Apples','Bananas','Strawberries','Rasberries','Blackberries','Cranberries','Apricots','Oranges','Peaches','Pears','Mangoes','Pineapples','Coconut']
+bone_types = ['Chicken wings', 'Chicken necks', 'Chicken legs', 'Chicken thighs', 'Turkey necks', 'Turkey', 'Lamb necks', 'Lamb ribs', 'Beef tail bones' ]
+organ_types = ['Kidneys', 'Lungs', 'Hearts', 'Brains']
+vegetable_types = ['Carrot', 'Sweet Potato', 'Peas', 'Broccoli', 'Celery', 'Green Beans', 'Cucumber', 'Cauliflower']
+seed_types = ['Flax','Chia','Pumpkin','Sunflower']
+
+class my_dictionary(dict):
+    def __init__(self):
+        self = dict()
+
+    def add(self, key, *value):
+        self[key] = value
+
+food_type = my_dictionary()
+
+food_type.add('Muscle Types', muscle_types )
+food_type.add('Fruit Types', fruit_types)
+food_type.add('Bone Types', bone_types)
+food_type.add('Organ Types', organ_types)
+food_type.add('Vegetable_types', vegetable_types)
+food_type.add('Seed_types', seed_types)
+
+
+print(food_type)
+
+
+foods_to_avoid = ['Onions' , 'Garlic', 'Leeks', 'Scallions', 'Kale', 'Wild Picked Mushrooms', 'Raw Potatoes']
 
